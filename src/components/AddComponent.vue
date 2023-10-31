e60dcf0ec86319148282d41729572f15acafe9c6
Shivani K <shivanik.bbi18@rvce.edu.in>
2023-10-31 14:07:51 +0530

    changes in add component

--- src/components/AddComponent.vue
+++ src/components/AddComponent.vue
@@ -19,10 +19,11 @@
                     <div class="mt-6">
                         <div class="font-semibold">Paid by</div>
                         <div class="mt-2">
-                            <div class="flex w-full border border-secondary items-center justify-between bg-neutral-100 p-3 rounded-md">
+                            <div
+                                class="flex w-full border border-secondary items-center justify-between bg-neutral-100 p-3 rounded-md">
                                 <div class="flex items-center gap-x-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" fill="none" class="stroke-primary w-6 h-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5"
+                                        stroke="currentColor" fill="none" class="stroke-primary w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round"
                                             d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                                     </svg>
@@ -46,7 +47,8 @@
                         <div class="font-semibold">Date</div>
                         <div class="mt-2">
                             <div>
-                                <input type="date" class="bg-neutral-100 border border-secondary rounded-md p-3 w-full" v-model="formData.date" />
+                                <input type="date" class="bg-neutral-100 border border-secondary rounded-md p-3 w-full"
+                                    v-model="formData.date" />
                             </div>
                         </div>
                     </div>
@@ -188,7 +190,7 @@ const closeAddPeopleDialog = () => {
 };
 
 const addSelectedPeople = () => {
-    const newPerson = {
+    const newPerson: Participant = {
         userId: selectedUserId.value,
         share: 0,
     };
@@ -202,7 +204,6 @@ const addSelectedPeople = () => {
     newPerson.share = equalShare;
     selectedPeople.value = [...selectedPeople.value, newPerson];
     selectedUserId.value = '';
-    shareInput.value = 0;
     closeAddPeopleDialog();
 };
 const removeSuggestion = (index: number) => {
@@ -251,6 +252,4 @@ onMounted(() => {
 });
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
